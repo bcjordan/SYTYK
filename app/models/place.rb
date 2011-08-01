478606101e346f6ce615a963bab84d7dbed0795d
Brian Jordan <bcjordan@gmail.com>
2011-08-01 13:25:01 -0400

    add seeding

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,8 @@ class Place < ActiveRecord::Base
 
   validates_uniqueness_of :location_id
 
+  # May be less strict with this when allowing for searched items
+  validates_presence_of :name, :location_name, :lat, :lng, :url, :image_url, :category, :location_id, :num_reviews, :ranking
+
   
 end
