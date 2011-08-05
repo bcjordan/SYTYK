f74675648fe8b76c0c7f7f4db82f51967eba67fc
Brian Jordan <bcjordan@gmail.com>
2011-08-05 08:52:43 -0400

    invalid param fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < ActionController::Base
   before_filter :init
 
   rescue_from Facebooker::Session::SessionExpired, :with => :facebook_session_expired
-#  rescue_from Facebooker::Session::MissingOrInvalidParameter, :with => :facebook_session_expired
+  rescue_from Facebooker::Session::MissingOrInvalidParameter, :with => :facebook_session_expired
 
   def facebook_session_expired
     clear_fb_cookies!
     clear_facebook_session_information
     reset_session
     flash[:error] = "Your facebook session has expired."
-    redirect_to "https://soyouthinkyouknow.heroku.com/?reset=1"
+    if ENV['RAILS_ENV'] == 'production'
+      redirect_to "https://soyouthinkyouknow.heroku.com/?reset=1"
+    else
+      redirect_to "http://0.0.0.0:3000/?reset=1"
+    end
   end
 
   def init
